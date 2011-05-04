7523f8e630d2b52aed41f5c97d926525f86b73f0
onze <onzeonline@gmail.com>
2011-05-04 17:58:34 -0400

    first commit
    Signed-off-by: onze <onzeonline@gmail.com>

---
+++ src/unsteamable.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : unsteamable.cpp
+// Author      : Valentin Bisson
+// Version     :
+// Copyright   : GLP3
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
